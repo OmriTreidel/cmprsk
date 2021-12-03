e96e1f33862f606f20a8db9bb719794a185a6666
Omri Treidel <treidel2@gmail.com>
2021-12-03 16:10:39 +1000

    update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cmprsk",
-    version="0.1.0",
+    version="1.0.0",
     author="Omri Treidel",
     author_email="treidel2@gmail.com",
     description="A python wrapper around cmprsk R package",
